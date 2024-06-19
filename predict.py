9c1f07ed6c3e8d860cea2e3bda9d6e0fd481c5d7
jianghongcheng <35664721+jianghongcheng@users.noreply.github.com>
2024-06-18 21:47:44 -0500

    Update predict.py

--- predict.py
+++ predict.py
@@ -1,115 +1,75 @@
-import os
 import torch
+import os
 import numpy as np
 from PIL import Image
-import zipfile
-from fw_sat_arch import FW_SAT
-from utils import Loss_PSNR, AverageMeter
-from pytorch_ssim import SSIM
+from fw_sat_arch import FW_SAT  # Assuming FW_SAT is defined in fw_sat_arch
+from pytorch_ssim import SSIM  # Assuming SSIM is imported from pytorch_ssim module
 
-def zip_files(directory_to_zip, output_zip_path):
-    """
-    Zips the contents of a directory.
 
-    Args:
-        directory_to_zip (str): Path to the directory containing files to zip.
-        output_zip_path (str): Path to the output zip file.
-    """
-    with zipfile.ZipFile(output_zip_path, 'w', zipfile.ZIP_DEFLATED) as zipf:
-        for root, _, files in os.walk(directory_to_zip):
-            for file in files:
-                file_path = os.path.join(root, file)
-                zipf.write(file_path, os.path.relpath(file_path, directory_to_zip))
+class ImageProcessor:
+    def __init__(self, model_path, rgb_path, lr_thermal_path, save_path):
+        self.model = FW_SAT().cuda()
+        self.model.load_state_dict(torch.load(model_path)['state_dict'])
+        self.rgb_path = rgb_path
+        self.lr_thermal_path = lr_thermal_path
+        self.save_path = save_path
 
-def load_model(checkpoint_path):
-    """
-    Loads the model from a checkpoint.
+    def process_images(self):
+        if not os.path.exists(self.save_path):
+            os.makedirs(self.save_path)
 
-    Args:
-        checkpoint_path (str): Path to the model checkpoint.
+        images = os.listdir(self.lr_thermal_path)
 
-    Returns:
-        torch.nn.Module: Loaded model.
-    """
-    model = FW_SAT().cuda()
-    checkpoint = torch.load(checkpoint_path)
-    model.load_state_dict(checkpoint['state_dict'])
-    return model
+        for idx, image_name in enumerate(images):
+            rgb, lr_thermal = self.load_images(image_name)
 
-def preprocess_image(image_path, expand_dims=True):
-    """
-    Preprocesses an image for model input.
+            output = self.predict(rgb, lr_thermal)
 
-    Args:
-        image_path (str): Path to the image.
-        expand_dims (bool): Whether to expand dimensions for batch size.
+            self.save_output(output, image_name)
 
-    Returns:
-        torch.Tensor: Preprocessed image tensor.
-    """
-    image = Image.open(image_path)
-    image = np.array(image) / 255.0
-    if len(image.shape) == 3:
-        image = np.transpose(image, (2, 0, 1))
-    else:
-        image = np.expand_dims(image, axis=0)
-    if expand_dims:
-        image = np.expand_dims(image, axis=0)
-    return torch.from_numpy(image).float()
+    def load_images(self, image_name):
+        rgb = Image.open(os.path.join(self.rgb_path, image_name.replace('_th', '_vis')))
+        lr_thermal = Image.open(os.path.join(self.lr_thermal_path, image_name))
+        
+        rgb_image = np.array(rgb) / 255.0
+        lr_thermal_image = np.array(lr_thermal) / 255.0
 
-def save_image(output, save_path, image_name):
-    """
-    Saves the output image to the specified path.
+        rgb_image = np.transpose(rgb_image, (2, 0, 1))
+        lr_thermal_image = np.expand_dims(lr_thermal_image[:, :, 0], axis=0)
 
-    Args:
-        output (torch.Tensor): Output image tensor.
-        save_path (str): Directory to save the image.
-        image_name (str): Name of the image file.
-    """
-    output = output.cpu().numpy().squeeze() * 255.0
-    output = output.astype(np.uint8)
-    output_image = Image.fromarray(output)
-    output_image.save(os.path.join(save_path, image_name))
+        rgb_image = np.expand_dims(rgb_image, axis=0)
+        lr_thermal_image = np.expand_dims(lr_thermal_image, axis=0)
 
-def process_images(model, rgb_dir, lr_thermal_dir, save_dir):
-    """
-    Processes images with the model and saves the output.
+        lr_thermal_image = torch.from_numpy(lr_thermal_image).float()
+        rgb_image = torch.from_numpy(rgb_image).float()
 
-    Args:
-        model (torch.nn.Module): The loaded model.
-        rgb_dir (str): Directory containing RGB images.
-        lr_thermal_dir (str): Directory containing low-resolution thermal images.
-        save_dir (str): Directory to save the processed images.
-    """
-    if not os.path.exists(save_dir):
-        os.makedirs(save_dir)
+        return rgb_image, lr_thermal_image
 
-    image_files = os.listdir(lr_thermal_dir)
-    for image_name in image_files:
-        rgb_image_path = os.path.join(rgb_dir, image_name.replace('_th', '_vis'))
-        lr_thermal_image_path = os.path.join(lr_thermal_dir, image_name)
-
-        rgb_image = preprocess_image(rgb_image_path)
-        lr_thermal_image = preprocess_image(lr_thermal_image_path, expand_dims=False)
-        lr_thermal_image = lr_thermal_image.unsqueeze(0)
-
+    def predict(self, rgb_image, lr_thermal_image):
         with torch.no_grad():
             rgb_image = rgb_image.cuda()
             lr_thermal_image = lr_thermal_image.cuda()
-            output = model(rgb_image, lr_thermal_image)
+            output = self.model(rgb_image, lr_thermal_image)
 
-        save_image(output, save_dir, image_name)
+        output = output.cpu().numpy()
+        output = np.squeeze(output) * 255.0
+        output = output.astype(np.uint8)
 
-def main():
-    checkpoint_path = '/media/max/a/2024_CVPRW_SR/FW_SAT/net_best_8.pth'
-    rgb_dir = '../data/visible/test/guided_x8/'
-    lr_thermal_dir = '../data/thermal/test/guided_x8/LR_x8/'
-    save_dir = '../data/test/'
+        return output
 
-    model = load_model(checkpoint_path)
-    process_images(model, rgb_dir, lr_thermal_dir, save_dir)
-    # Uncomment the line below to zip the output files
-    # zip_files(save_dir, '../data/evaluation.zip')
+    def save_output(self, output, image_name):
+        output_image = Image.fromarray(output)
+        output_image.save(os.path.join(self.save_path, image_name))
 
-if __name__ == '__main__':
-    main()
+
+
+
+if __name__ == "__main__":
+    model_path = '/media/max/a/2024_CVPRW_SR/FW_SAT/FW_SAT/exp/net_32epoch.pth'
+    rgb_path = '../data/visible/test/guided_x8/'
+    lr_thermal_path = '../data/thermal/test/guided_x8/LR_x8/'
+    save_path = '../data/test_out/'
+
+    image_processor = ImageProcessor(model_path, rgb_path, lr_thermal_path, save_path)
+    image_processor.process_images()
+
